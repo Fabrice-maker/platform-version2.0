5e847c107ba36012e7ccff284498d553175ec8f0
Fabrice-maker <56657118+Fabrice-maker@users.noreply.github.com>
2020-01-22 17:51:20 -0500

    Add files via upload

---
+++ canvas.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById("display")
+
+console.log(canvas)
+
+canvas.width = document.body.clientWidth
+canvas.Height = document.body.clientHeight
+let ctx = canvas.getContext("2d")
+
+console.log(ctx)
+
+ctx.fillStyle = "cyan"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+ctx.fillStyle = "black"
+ctx.fillRect(0, 0, canvas.width, canvas.height / 2)
+ctx.fillStyle = "white"
+ctx.fillRect(0, 0, canvas.width / 2, canvas.height)
+ctx.fillStyle = "red"
+ctx.fillRect(0, 0, canvas.width / 2, canvas.height / 2)
