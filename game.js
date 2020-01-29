f09a17852cd330d63ee381d8b8400119de4d25e1
Fabrice-maker <56657118+Fabrice-maker@users.noreply.github.com>
2020-01-29 18:00:56 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -2,14 +2,17 @@
 window.addEventListener("keydown", event => {
 	let colorIndex = Math.random()
 	//let color = "red"
-	if (colorIndex > 0.25 && 0.50) {
+	if (colorIndex > 0.10 && 0.30) {
 		color = "blue"
     }
-	if (colorIndex < 0.25) {
+	if (colorIndex < 0.10) {
 		color = "green"
 	}
-	if (colorIndex > 0.50 ) {
+	if (colorIndex > 0.30 && 0.6) {
 		color = "red"
+    }
+    if (colorIndex > 0.60 ) {
+		color = "cyan"
     }
 	ctx.fillStyle = color
 
